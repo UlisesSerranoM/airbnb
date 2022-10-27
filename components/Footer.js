fc6a62e6024ba7e2e4c32d3c85192d92acb2aa1b
UlisesSerrano-vector <userrano@vector.com.mx>
2022-10-27 09:46:02 -0500

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,48 @@
+
+function Footer() {
+
+    return(
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32
+        py-14 bg-gray-100 text-gray-600 dark:bg-black">
+            <div className="space-y-4 text-xs text-gray-800 dark:text-white">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbn Luxe</p>
+            </div>
+            
+            <div>
+                <div className="space-y-4 text-xs text-gray-800 dark:text-white">
+                    <h5 className="font-bold">COMMUNITY</h5>
+                    <p>This is not a real site</p>
+                    <p>This clone looks awesome</p>
+                    <p>Refarrals aceppted</p>
+                    <p> Ulises Rocks</p>
+                    <p>You want a web page like this?</p>
+                </div>
+            </div>
+                <div className="space-y-4 text-xs text-gray-800 dark:text-white">
+                    <h5 className="font-bold">About</h5>
+                    <p>How Airbnb works</p>
+                    <p>Newsroom</p>
+                    <p>Investors</p>
+                    <p>Airbnb Plus</p>
+                    <p>Airbn Luxe</p>
+                </div>
+            <div>
+                <div className="space-y-4 text-xs text-gray-800  dark:text-white">
+                    <h5 className="font-bold">SOCIAL MEDIA</h5>
+                    <p>How Airbnb works</p>
+                    <p>Newsroom</p>
+                    <p>Investors</p>
+                    <p>Airbnb Plus</p>
+                    <p>Airbn Luxe</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Header.js
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import {
+    MagnifyingGlassIcon,
+    UserCircleIcon,
+    Bars3Icon,
+    UsersIcon 
+} from "@heroicons/react/24/solid"
+import {
+    GlobeAltIcon,
+} from "@heroicons/react/24/outline"
+function Header(){
+    return(
+        <header className="sticky top-0 z-50 grid
+        grid-cols-3 bg-white dark:bg-black shadow-md p-5 md:px-10">
+           {/*  left */}
+           {/* el md: es para que agarre esa carateristica en patanllas medianas
+           relative flex es para que se quede en esa posicion sin importar lo de afuera
+           cursosr pointer para que aparezca el cursor cuando el usuario quiera darle click
+            */}
+            <div className="relative flex items-center h-10
+            cursor-pointer my-auto">
+                <Image src="https://links.papareact.com/qd3"
+                 layout="fill"
+                 objectfit ="contain"
+                 objectposition="left"
+                 width={"100"}
+                 height={"100"}
+                />
+            </div>
+            {/*center  search*/}
+            <div className="flex items-center md:border-2
+            rounded-full py-2 md:shadow-sm">
+                <input type="text" placeholder="Start your
+                search" className="flex-grow pl-5 bg-transparent outline-none text-gray-600
+                text-sm placeholder-gray-400"/>
+                <MagnifyingGlassIcon  className="hidden md:inline-flex h-8 bg-red-400
+                text-white rounded-full p-2 cursor-pointer md:mx-2"/>
+            </div>
+
+            {/* right */}
+            <div className="flex items-center space-x-4 justify-end text-gray-500">
+                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <GlobeAltIcon className="h-6 cursor-pointer"/>
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                    <Bars3Icon className="h-6 cursor-pointer"/>
+                    <UserCircleIcon className="h-6 cursor-pointer" />
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
+
